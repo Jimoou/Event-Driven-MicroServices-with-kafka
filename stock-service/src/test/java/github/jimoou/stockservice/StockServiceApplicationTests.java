f89ccde47a891ee553a12d610ea7f43a7aec6e35
Jiwoon <kfromh0136@Naver.com>
2023-08-02 09:55:41 +0900

    [Chore] Create 4 MicroServices
    BaseDomains
    OrderService
    StockService
    EmailService

---
+++ stock-service/src/test/java/github/jimoou/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package github.jimoou.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
